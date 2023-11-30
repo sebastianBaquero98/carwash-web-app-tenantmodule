0acbd1da926b104eca11b45ecda42d19cf9bdd1d
Sebastian Baquero <46967870+sebastianBaquero98@users.noreply.github.com>
2023-11-30 10:52:34 -0500

    Country field for tenant creation added and field validations

--- components/TenantCreationForm.jsx
+++ components/TenantCreationForm.jsx
@@ -2,15 +2,19 @@
 
 import { useState, useEffect } from "react";
 import { Input } from 'reactstrap';
-import Image from "next/image"
+import Image from "next/image";
+import Select from 'react-select';
 
 const TenantCreationForm = () => {
     const [tenantEmail,setTenantEmail] = useState('');
     const [tenantName,setTenantName] = useState('');
+    const [tenantCountry,setTenantCountry] = useState('');
     const [tenantPhone,setTenantPhone] = useState('');
     const [tenantPersonInCharge,setTenantPersonInCharge] = useState('');
     const [isLoadingCreation, setIsLoadingCreation] = useState(false);
     const [tenantCreated, setTenantCreated] = useState(false);
+    const [filledAllInputs, setFilledAllInputs] = useState(false);
+    const [enterdSend, setEnteredSend] = useState(false);
 
     // const delay = ms => new Promise(
     //     resolve => setTimeout(resolve, ms)
@@ -28,48 +32,92 @@ const TenantCreationForm = () => {
     //           makeRequest();
     //     }
     // },[tenantCreated])
+    useEffect(()=>{
+        console.log(filledAllInputs)
+    },[filledAllInputs])
+
+    const countryOptions = [
+        { value: 'COLOMBIA', label: 'COLOMBIA' },
+        { value: 'MEXICO', label: 'MEXICO' },
+        { value: 'USA', label: 'USA' }
+    ]
+
+    const customStyles = {
+        option: (defaultStyles, state) => ({
+          ...defaultStyles,
+          color: state.isSelected ? "#D9D9D9" : "#219EBC",
+          backgroundColor: state.isSelected ? "#219EBC" : "#fff",
+        }),
+    
+        control: (defaultStyles) => ({
+          ...defaultStyles,
+          backgroundColor: "#219EBC",
+          padding: "1px",
+          border: "none",
+          boxShadow: "none",
+          borderRadius:"10px"
+        }),
+        singleValue: (defaultStyles) => ({ ...defaultStyles, color: "#F4F5F9" }),
+    };
 
     const handleChangeTE = (event)=>{
+        setEnteredSend(false)
         setTenantEmail(event.target.value)
     }
 
     const handleChangeTN = (event)=>{
+        setEnteredSend(false)
         setTenantName(event.target.value)
     }
 
     const handleChangeTP = (event)=>{
+        setEnteredSend(false)
         setTenantPhone(event.target.value)
     }
 
     const handleChangeTPIC = (event)=>{
+        setEnteredSend(false)
         setTenantPersonInCharge(event.target.value)
+    }
+    const handleChangeCountry= (selectedOption) =>{
+        setEnteredSend(false)
+        setTenantCountry(selectedOption)
+    }
+    const checkIfAllInputs = ()=>{
+        setFilledAllInputs( tenantCountry !== '' && tenantPersonInCharge !== '' && tenantPhone !== '' && tenantName !== '' && tenantEmail !== '')
     }
 
     const createTenant = ()=>{
-        var myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
-
-        var raw = JSON.stringify({
+        checkIfAllInputs()
+        setEnteredSend(true)
+        if(filledAllInputs){
+            var myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+    
+            var raw = JSON.stringify({
                 "tenantEmail": tenantEmail,
                 "tenantName": tenantName,
                 "tenantPhone": tenantPhone,
-                "name": tenantPersonInCharge
-                });
-
-                var requestOptions = {
-                method: 'POST',
-                headers: myHeaders,
-                body: raw,
-                redirect: 'follow'
-                };
-                setIsLoadingCreation(true)
-                fetch("https://afmlbia6k2.execute-api.us-east-1.amazonaws.com/development/registration", requestOptions)
-                .then(response => response.text())
-                .then(result => {
-                    setIsLoadingCreation(false);
-                    setTenantCreated(true);
-                })
-                .catch(error => console.log('error', error));
+                "name": tenantPersonInCharge,
+                "tenantCountry": tenantCountry.value
+            });
+    
+            var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+            };
+            setIsLoadingCreation(true)
+            fetch("https://afmlbia6k2.execute-api.us-east-1.amazonaws.com/development/registration", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                setIsLoadingCreation(false);
+                setTenantCreated(true);
+            })
+            .catch(error => console.log('error', error));
+        }
+       
     }
 
   return (
@@ -93,7 +141,7 @@ const TenantCreationForm = () => {
                         <Image alt="form-name-icon"src={'/assets/images/form-edit-icon.svg'} width={30} height={30} />
                         <label htmlFor="">Tenant Email</label>
                     </div>
-                    <Input type="text" onChange={handleChangeTE} value={tenantEmail} name="name" id="name" placeholder="Tenant Email" autofocus="autofocus" />
+                    <Input type="email" onChange={handleChangeTE} value={tenantEmail} name="name" id="name" placeholder="Tenant Email" autofocus="autofocus" />
                 </div>
 
                 <div className="new-user-row">
@@ -111,10 +159,21 @@ const TenantCreationForm = () => {
                     </div>
                     <Input type="text" onChange={handleChangeTPIC} value={tenantPersonInCharge} name="name" id="name" placeholder="Name Person In Charge" autofocus="autofocus" />
                 </div>
+                <div className="new-user-row">
+                    <Select
+                        defaultValue={{value:' ', label:'Select'}}
+                        name="Tenant Country"
+                        onChange={handleChangeCountry}
+                        options={countryOptions}
+                        styles={customStyles}
+                    />
+                </div>
+                
 
                 <div id="button-form-box" className="new-user-row">
                 {/* {userAlreadyexists && <p style={{color:'red'}}>User already exists</p>} */}
                 {tenantCreated && <p style={{color:'#023047'}}>Tenant Created</p>}
+                {(!filledAllInputs && enterdSend ) && <p style={{color:'red'}}>Please Fill in all fields</p>}
                 {isLoadingCreation && <p style={{color:'#023047'}}>Loading</p>} 
                     <input onClick={createTenant} type="button" name="add-user" id="add-user" value="Add User" />
                 </div>
